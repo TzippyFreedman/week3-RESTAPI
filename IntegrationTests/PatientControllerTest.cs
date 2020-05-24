6384c3b7ff82983a2148a6b32a755534d431fab9
estherRivka <estherivka1@gmail.com>
2020-05-24 17:23:31 +0300

    check test

---
+++ IntegrationTests/PatientControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+
+
+namespace CoronaApp.Tests
+{
+
+        public class PatientControllerTests : IClassFixture<WebApplicationFactory>
+        {
+            private readonly WebApplicationFactory<Api.Startup> _factory;
+            public PatientControllerTests(WebApplicationFactory<Api.Startup> factory)
+            {
+                _factory = factory;
+            }
+
+            [Fact]
+            public async void GetPatient_ById_ReturnPatient()
+            {
+                // Arrange
+                var client = _factory.CreateClient();
+
+                // Act
+                var response = await client.GetAsync("/api/patient/000000018");
+
+                // Assert
+                response.EnsureSuccessStatusCode();
+            }
+        }
+    }
+
